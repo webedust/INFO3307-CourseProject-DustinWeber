09dc4a2c3785c3133a756b84fbe055d957c8ada2
Dustin Weber <webedust@isu.edu>
2020-09-17 12:24:26 -0600

    Basic File IO & Tests (fixed) - Actually pushed files this time.

--- TodoManager/Form1.Designer.cs
+++ TodoManager/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.ButtonSave = new System.Windows.Forms.Button();
+            this.Textbox = new System.Windows.Forms.RichTextBox();
+            this.OutputTextbox = new System.Windows.Forms.RichTextBox();
+            this.ButtonLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonSave
+            // 
+            this.ButtonSave.Location = new System.Drawing.Point(582, 193);
+            this.ButtonSave.Name = "ButtonSave";
+            this.ButtonSave.Size = new System.Drawing.Size(60, 35);
+            this.ButtonSave.TabIndex = 0;
+            this.ButtonSave.Text = "Save file";
+            this.ButtonSave.UseVisualStyleBackColor = true;
+            this.ButtonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // Textbox
+            // 
+            this.Textbox.Location = new System.Drawing.Point(296, 77);
+            this.Textbox.Name = "Textbox";
+            this.Textbox.Size = new System.Drawing.Size(174, 72);
+            this.Textbox.TabIndex = 1;
+            this.Textbox.Text = "";
+            // 
+            // OutputTextbox
+            // 
+            this.OutputTextbox.Location = new System.Drawing.Point(582, 253);
+            this.OutputTextbox.Name = "OutputTextbox";
+            this.OutputTextbox.Size = new System.Drawing.Size(201, 178);
+            this.OutputTextbox.TabIndex = 2;
+            this.OutputTextbox.Text = "";
+            // 
+            // ButtonLoad
+            // 
+            this.ButtonLoad.Location = new System.Drawing.Point(667, 193);
+            this.ButtonLoad.Name = "ButtonLoad";
+            this.ButtonLoad.Size = new System.Drawing.Size(60, 35);
+            this.ButtonLoad.TabIndex = 0;
+            this.ButtonLoad.Text = "Load file";
+            this.ButtonLoad.UseVisualStyleBackColor = true;
+            this.ButtonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonLoad);
+            this.Controls.Add(this.OutputTextbox);
+            this.Controls.Add(this.Textbox);
+            this.Controls.Add(this.ButtonSave);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ButtonSave;
+        private System.Windows.Forms.RichTextBox Textbox;
+        private System.Windows.Forms.RichTextBox OutputTextbox;
+        private System.Windows.Forms.Button ButtonLoad;
     }
 }
 
--- TodoManager/Form1.cs
+++ TodoManager/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace TodoManager
@@ -17,5 +11,34 @@ namespace TodoManager
             InitializeComponent();
         }
 
+        void ButtonSave_Click(object sender, EventArgs e)
+        {
+            // Check if directory exists, create if it does NOT exist
+            if (!Directory.Exists(Application.StartupPath + "\\Tasks\\"))
+            {
+                Directory.CreateDirectory(Application.StartupPath + "\\Tasks\\");
+            }
+            // Create new stream in the directory where this was started from
+            StreamWriter stream = new StreamWriter(Application.StartupPath + "\\Tasks\\" + "TestTask.txt");
+            stream.WriteLine("Label could go here: " + Textbox.Text);
+            stream.Close();
+        }
+
+        void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            string dir = Application.StartupPath + "Tasks";
+            string path = dir + "\\TestTask.txt";
+            // Ensure file exists
+            if (File.Exists(path))
+            {
+                StreamReader stream = new StreamReader(path);
+                OutputTextbox.Text = stream.ReadToEnd();
+                stream.Close();
+            }
+            else // File does not exist
+            {
+                OutputTextbox.Text = "File " + path + " does not exist.";
+            }
+        }
     }
 }
